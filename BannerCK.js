65b9efa004d831d6eb7aa48b4ea1874c2334f156
Geverson-commits-to <72303804+Geverson-commits-to@users.noreply.github.com>
2020-10-03 18:13:17 -0300

    Add files via upload

---
+++ BannerCK.js
@@ -0,0 +1,14 @@
+const gol = [];
+    gol[0] = "foto1.jpg";
+    gol[1] = "foto2.png";
+    gol[2] = "foto3.jpg";
+     function MudarBanner(){
+         document.IMGbanner.src = gol[0];
+}
+     function MudarBanne(){
+         document.IMGbanner.src = gol[1];
+}
+     function MudarBann(){
+        document.IMGbanner.src = gol[2];
+}
+
